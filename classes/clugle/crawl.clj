6507b50402ed40be0139af168796934d3d997e2f
Joe DAlessandro <joe@thejoeshow.net>
2011-12-06 21:06:22 -0500

    removed classes directory

--- classes/clugle/crawl.clj
+++
@@ -1,8 +0,0 @@
-(ns clugle.crawl
-  (:require [clj-http.client :as client] 
-            [pl.danieljanus.tagsoup :as tagsoup]))
-
-;; returns some delicious HTML soup
-(defn soup-it [url]
-  (let [{body :body} (client/get url)]
-    (tagsoup/parse-string body)))
