8c18be0e0b61ac2f469a7001bd8189074cdbef19
Joe D'Alessandro <joe@thejoeshow.net>
2023-02-25 05:26:32 -0500

    updated project to use more recent config + LICENSE

--- project.clj
+++ project.clj
@@ -1,6 +1,14 @@
-(defproject clugle "1.0.0"
+(defproject clojure-fun "1.0"
   :description "quick, learn!"
+  :url "https://github.com/xupit3r/clugle"
+  :license {:name "EPL-2.0 OR GPL-2.0-or-later WITH Classpath-exception-2.0"
+            :url "https://www.eclipse.org/legal/epl-2.0/"}
   :dependencies [[org.clojure/clojure "1.11.1"]
                  [clj-http "3.12.3"]
-                 [clj-tagsoup "0.3.0"]
-                 [cheshire "5.11.0"]])
+                 [clj-tagsoup "0.3.0"] 
+                 [cheshire "5.11.0"]
+                 [compojure "1.7.0"] 
+                 [http-kit "2.6.0"]
+                 [ring/ring-defaults "0.3.4"]]
+  :repl-options {:init-ns clugle.core}
+  :main clugle.core)
