210ab308c09c2b6f74801f6857c4c7a80d691585
Joe D'Alessandro <joe@thejoeshow.net>
2023-02-28 03:25:30 -0500

    use refer to pull in the server

--- src/clugle/core.clj
+++ src/clugle/core.clj
@@ -1,5 +1,5 @@
 (ns clugle.core
-  (:require [org.httpkit.server :as server]
+  (:require [org.httpkit.server :refer [run-server]]
             [ring.middleware.defaults :refer [wrap-defaults, site-defaults]]
             [clugle.api.base :refer [api-routes]])
   (:gen-class))
@@ -8,5 +8,5 @@
   "main entry point - starts the API server"
   [& args]
   (let [port (Integer/parseInt (or (System/getenv "PORT") "3000"))]
-    (server/run-server (wrap-defaults #'api-routes site-defaults) {:port port})
+    (run-server (wrap-defaults #'api-routes site-defaults) {:port port})
     (println (str "Running webserver at http://127.0.0.1:" port "/"))))
