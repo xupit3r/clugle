4cf5a06490df4061012cec3c870aae3620643184
Joe D'Alessandro <joe@thejoeshow.net>
2023-03-03 04:14:14 -0500

    removed a bunch of functions that will never be used

--- src/clugle/util/hlpr.clj
+++ src/clugle/util/hlpr.clj
@@ -10,33 +10,6 @@
 (defn maxv [lst]
   (apply max lst))
 
-;; remove empty lists from a provided list
-(defn drop-empties
-  ([lst] (drop-empties lst '()))
-  ([lst acc]
-    (if (first lst)
-      (drop-empties (rest lst) (conj (first lst) acc))
-      (if (not (empty? lst))
-        (drop-empties (rest lst) acc)
-        acc))))
-
-;; get the size of a collection
-(defn size
-  ([col] (size col 0))
-  ([col cnt] 
-    (if (not (empty? col))
-      (size (rest col) (inc cnt))
-      cnt)))
-
-;; a listy item is one that 
-;; can be treated somewhat 
-;; as a list
-(defn listy? [something]
-  (or (list? something)
-      (vector? something)
-      (set? something)
-      (seq? something)))
-
 ;; returns a random permutation of
 ;; the provided list
 (defn shuffem [lst]
