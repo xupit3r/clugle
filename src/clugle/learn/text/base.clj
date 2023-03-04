33e7f580a19ab715a6fcba77139af8b6bec5d3f3
Joe D'Alessandro <joe@thejoeshow.net>
2023-03-04 04:57:01 -0500

    some refactoring  * created a utils module in text  * moved more utility-ish things to there

--- src/clugle/learn/text/base.clj
+++ src/clugle/learn/text/base.clj
@@ -1,47 +1,8 @@
 (ns clugle.learn.text.base
-  (:require [clojure.string :as str]
-            [clojure.math :as math]
-            [clojure.java.io :refer [file]]
-            [babashka.fs :refer [directory?]]
-            [clugle.util.hlpr :refer [maxv apply-mf vec-range]]))
+  (:require [clojure.math :as math]
+            [clugle.util.hlpr :refer [maxv apply-mf vec-range]]
+            [clugle.learn.text.utils :refer [clean-text tokenize]]))
 
-;;;; logic for doing stuff with text
-
-;; return a string representing 
-;; the desired delimiter
-(defn delim [dkey]
-  (cond (= dkey :space) #"\s"
-        (= dkey :pipe) #"\|"
-        (= dkey :pound) #"#"
-        (= dkey :percent) #"%"
-        (= dkey :tab) #"\t"
-        (= dkey :caret) #"\^"
-        (= dkey :period) #"\."
-        (= dkey :comma) #","))
-
-;; super simple for right now...
-(defn clean-text [str]
-  (str/replace str #"\." ""))
-
-;; reads files into a list of strings
-(defn doc-arr [docs] 
-  (map slurp 
-       (filter (fn [d] (not (directory? d))) docs)))
-
-;; read in all the files within a directory
-;; this reads them into an array of strings,
-;; so for directories with lotsa files...
-;; umm don't use this probably :)
-(defn load-docs [dir]
-  (-> dir file file-seq doc-arr vec))
-
-;; creates a vector of tuples 
-;; the first of each tuple is the 
-;; token, the second is a talley of 
-;; 1 (you will see what it is being used for)
-(defn tokenize
-  ([txt] (tokenize txt :space))
-  ([txt delimiter] (str/split txt (delim delimiter))))
 
 ;; builds a frequency count
 ;; of words appearing in a string
