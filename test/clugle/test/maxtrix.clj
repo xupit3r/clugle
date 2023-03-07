991279a2c05978fe058e93b24dd23f2f45190639
Joe D'Alessandro <joe@thejoeshow.net>
2023-03-07 05:13:59 -0500

    added another convolution test

--- test/clugle/test/maxtrix.clj
+++ test/clugle/test/maxtrix.clj
@@ -15,4 +15,11 @@
         input  [1 2 3 4 5]]
     (is (=
          (convolution window input)
-         [3 8 14 20 26 14 5]))))
+         [3 8 14 20 26 14 5]))
+    (is (=
+         (apply
+          max
+          (convolution
+           [0.05 0.03 0.01]
+           [10 20 30 20 10 10 10]))
+         2.2))))
