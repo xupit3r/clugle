014055a03642ad01e66437812e17b62d8be0985b
Joe D'Alessandro <joe@thejoeshow.net>
2023-03-07 04:51:04 -0500

    implemented a basic convolution application...

--- src/clugle/learn/matrix/base.clj
+++ src/clugle/learn/matrix/base.clj
@@ -20,3 +20,39 @@
    (fn [input]
      (mapv (apply-op input) ops))
    inputs))
+
+
+;; given a vector, an iteration, and a desired length
+;; this will create an appropriate sliding window padding
+;; to allow for matrix operation
+(defn conv-op [v i len]
+  (let [offset (abs (- (count v) i))]
+    (vec
+     (flatten
+      (if (<= i (count v))
+        (conj
+         (vec
+          (repeat (- len i) 0)) (subvec v 0 i))
+        (conj
+         (vec
+          (repeat (- len (count v) offset) 0))
+         v
+         (vec
+          (repeat offset 0))))))))
+
+;; prepares input data for a convolution operation
+(defn prep-conv-input [window input]
+  (vec
+   (flatten
+    (conj (vec (repeat (count window) 0))
+          (reverse input)))))
+
+;; performs a convolution between a supplied
+;; window and input to apply the window to
+(defn convolution [window input]
+  (let [iterations (+ (count window) (count input))
+        ivec (prep-conv-input window input)]
+        (vec
+         (for [i (range 1 iterations)
+               :let [op (conv-op window i iterations)]]
+           (apply + (mapv * op ivec))))))
