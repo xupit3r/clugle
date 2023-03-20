445fd8705dd88caef8fd2ad2ec07d452383fc052
Joe D'Alessandro <joe@thejoeshow.net>
2023-03-20 04:52:47 -0400

    WIP: added simple testing pipeline tools  * generic runner  * generic reporter

---
+++ src/clugle/learn/testing/base.clj
@@ -0,0 +1,29 @@
+(ns clugle.learn.testing.base)
+
+;; generic function to run a simple
+;; predictor against a dataset and
+;; return some simple results
+(defn runner [{dataset :dataset 
+               predictor :predictor
+               skey :skey}]
+  (reduce
+   #(assoc
+     %1
+     :total (inc (:total %1))
+     :correct (+ (:correct %1)
+                 (if (= (%2 0) (%2 1)) 1 0)))
+   {:total 0 :correct 0}
+   (mapv
+    #(vec [(skey %1) (predictor %1)])
+    dataset)))
+
+;; generates a report of the run
+(defn reporter [{total :total correct :correct}]
+  (let [accuracy (/ correct total)]
+    {:total total
+     :correct correct
+     :accuracy accuracy
+     :message (format
+               "got %.3f%% correct of %d samples"
+               (float (* 100 accuracy))
+               total)}))
--- src/clugle/learn/testing/sentiment.clj
+++ src/clugle/learn/testing/sentiment.clj
@@ -1,5 +1,6 @@
 (ns clugle.learn.testing.sentiment
   (:require [clugle.learn.data.datasets :refer [load-dataset]]
+            [clugle.learn.testing.base :refer [runner reporter]]
             [clugle.learn.text.preprocess :refer [normalize]]
             [clugle.learn.text.sentiment :refer [lexicon-score]]))
 
@@ -12,13 +13,8 @@
           :else "neutral"))))
 
 (defn lexicon []
-  (let [dataset (load-dataset :twitter-airline)]
-    (reduce
-     #(assoc
-       %1
-       :total (inc (:total %1))
-       :correct (+ (:correct %1) (if (= (%2 0) (%2 1)) 1 0)))
-     {:total 0 :correct 0}
-     (mapv
-      #(vec [(:airline_sentiment %1) (get-lexicon-sentiment %1)])
-      dataset))))
+  (-> {:dataset (load-dataset :twitter-airline)
+       :predictor get-lexicon-sentiment
+       :skey :airline_sentiment}
+      runner
+      reporter))
