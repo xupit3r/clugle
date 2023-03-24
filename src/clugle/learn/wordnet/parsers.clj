dcf829ab76ad4ca0974dc42a3e9520e3dcdf92fa
Joe D'Alessandro <joe@thejoeshow.net>
2023-03-24 04:08:19 -0400

    parsing for the data files  * handle the word lists and references  * generalized where i could

--- src/clugle/learn/wordnet/parsers.clj
+++ src/clugle/learn/wordnet/parsers.clj
@@ -15,6 +15,33 @@
      :tagsense_cnt (Integer/parseInt (tokens (+ 5 p_cnt)))
      :synset_offset (Integer/parseInt (tokens (+ 6 p_cnt)))}))
 
+;; creates groups of size n for a supplied
+;; collection
+(defn groups [n col]
+  (mapv
+   #(subvec col % (+ % n))
+   (take (Math/floor (/ (count col) n))
+         (iterate #(+ % n) 0))))
+
+;; parses the words, creating a hashmap
+;; containing the word and the lookup
+;; in the lex file
+(defn parse-words [col]
+  (->> col
+      (groups 2)
+      (mapv #(hash-map 
+              :word (get % 0) 
+              :lookup (str/join %)))))
+
+(defn parse-refs [col]
+  (->> col
+       (groups 4)
+       (mapv #(hash-map
+               :symbol (get % 0)
+               :offset (get % 1)
+               :pos (get % 2)
+               :target (get % 3)))))
+
 ;; parses a line from a data file 
 (defn data-parser [line]
   (let [[lex gloss] (str/split line #"\|")
@@ -25,12 +52,13 @@
         p_sets (* p_cnt 4)]
     {:synset_offset (Integer/parseInt (tokens 0))
      :lex_filename (lexnames (tokens 1))
-     :ss_type (tokens 2)
-     :words (subvec tokens
-                    (if (> w_cnt 0) 4 3)
-                    (+ 4 w_pairs))
-     :refs (subvec tokens 
-                   (if (> p_cnt 0) (+ 5 w_pairs) (+ 4 w_pairs)) 
-                   (+ 5 w_pairs p_sets))
-     :gloss (str/trim gloss)
-    }))
+     :pos (tokens 2)
+     :words (parse-words
+               (subvec tokens
+                       (if (> w_cnt 0) 4 3)
+                       (+ 4 w_pairs)))
+     :refs (parse-refs
+            (subvec tokens
+                    (if (> p_cnt 0) (+ 5 w_pairs) (+ 4 w_pairs))
+                    (+ 5 w_pairs p_sets)))
+     :gloss (str/trim gloss)}))
