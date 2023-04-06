53329ac001418ca8a57c2ff3cdb0ad588c33f333
Joe D'Alessandro <joe@thejoeshow.net>
2023-04-06 07:34:33 -0400

    added some sentiment scoring

--- src/clugle/learn/text/sentcore.clj
+++ src/clugle/learn/text/sentcore.clj
@@ -1,4 +1,5 @@
 (ns clugle.learn.text.sentcore
+  (:require [clojure.string :as str])
   (:import [java.util
             Properties]
            [edu.stanford.nlp.pipeline
@@ -8,6 +9,14 @@
 ;; sentiment directed config
 (def annotators "tokenize,ssplit,pos,parse,sentiment")
 (def algorithm "neural")
+
+;; maps the sentiment classes to a numeric
+;; score
+(def SCORES {"very_positive" 2
+             "positive" 1
+             "neutral" 0
+             "negative" -1
+             "very_negative" -2})
 
 ;; build a pipeline to annotate the text
 (defn get-pipeline []
@@ -23,10 +32,24 @@
     (.annotate pipeline document)
     document))
 
+;; generates a score between -1 and 1
+;; -1 being very negative
+;;  1 being very positive
+;;  0 being generally neutral...
+(defn genscore [groups]
+  (let [max (* (apply + (map #(count %) (vals groups))) 2)]
+    (/ (reduce-kv
+        #(+ %1 (* (SCORES (str/lower-case %2)) (count %3)))
+        0
+        groups)
+       max)))
+
 ;; get the per-sentence sentiment breakdown
 ;; of the text
 (defn senttext [text]
   (->> text
        annotate
        .sentences
-       (map #(.sentiment %))))
+       (map #(.sentiment %))
+       (group-by identity)
+       (genscore)))
